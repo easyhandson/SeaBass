e58db13c625b4e0bb0226250eb538cddf3d947a1
guobinkai <guobinkai@pconline.com.cn>
2023-06-06 18:09:35 +0800

    fix: 初始化SeaBass项目

---
+++ seabass-example/src/test/java/cn/com/seabase/example/ExampleTest.java
@@ -0,0 +1,15 @@
+package cn.com.seabase.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试类
+ */
+public class ExampleTest {
+    
+    @Test
+    public void Test() {
+        System.out.println("hell word");
+    }
+    
+}
