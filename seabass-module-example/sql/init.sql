8f732f9ce39c1eed183e2d42d2e4f1fa7ea65f12
guobinkai <guobinkai@pconline.com.cn>
2023-06-21 16:23:31 +0800

    feat: 重写初始化sql脚本

--- seabass-module-example/sql/init.sql
+++ seabass-module-example/sql/init.sql
@@ -1,38 +1,84 @@
-/* 建库 seabass_example */
-DROP DATABASE IF EXISTS `seabass_example`;
-CREATE DATABASE  `seabass_example` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+/* -------------------------------- 建库 seabass_user -------------------------------- */
+DROP DATABASE IF EXISTS `seabass_user`;
+CREATE DATABASE  `seabass_user` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-USE seabass_example;
+USE `seabass_user`;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 /* 活动表 */
-DROP TABLE IF EXISTS `seabass_example_act`;
-CREATE TABLE `seabass_example_act` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `create_time` datetime NOT NULL DEFAULT 0 COMMENT '创建时间',
-    `update_time` datetime NOT NULL DEFAULT 0 COMMENT '更新时间',
-    `act_name` varchar(50) NOT NULL DEFAULT '' COMMENT '活动名称',
-    `begin_time` datetime NOT NULL DEFAULT 0 COMMENT '开始时间',
-    `end_time` datetime NOT NULL DEFAULT 0 COMMENT '结束时间',
-    `sequence` int NOT NULL DEFAULT 0 COMMENT '序号',
-    PRIMARY KEY (`id`),
-    KEY `idx_sequence_create_time` (`sequence`, `create_time`)
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT='活动表';
+    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `update_time` datetime NOT NULL COMMENT '更新时间',
+    `nickname` VARCHAR(100) NOT NULL COMMENT '昵称',
+    `balance` DECIMAL(10, 0) NOT NULL DEFAULT 0 COMMENT '余额',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT='产品表';
 
-/* 测试表初始化语句 */
+/* 清空表数据并初始化 */
 BEGIN;
-/* 清空表数据并初始化问题 */
-TRUNCATE TABLE `seabass_example_act`;
-INSERT INTO `seabass_example_act` (create_time, update_time, act_name, begin_time, end_time, sequence) VALUES
-(now(), now(), '活动1', UNIX_TIMESTAMP('2022-06-11 00:00:00'), UNIX_TIMESTAMP('2022-06-18 23:59:59'), 1),
-(now(), now(), '活动2', UNIX_TIMESTAMP('2022-06-11 00:00:00'), UNIX_TIMESTAMP('2022-06-18 23:59:59'), 2),
-(now(), now(), '活动3', UNIX_TIMESTAMP('2022-06-14 00:00:00'), UNIX_TIMESTAMP('2022-06-20 23:59:59'), 3),
-(now(), now(), '活动4', UNIX_TIMESTAMP('2022-06-14 00:00:00'), UNIX_TIMESTAMP('2022-06-20 23:59:59'), 4),
-(now(), now(), '活动5', UNIX_TIMESTAMP('2022-06-18 00:00:00'), UNIX_TIMESTAMP('2022-06-28 23:59:59'), 5),
-(now(), now(), '活动6', UNIX_TIMESTAMP('2022-06-18 00:00:00'), UNIX_TIMESTAMP('2022-06-28 23:59:59'), 6);
+TRUNCATE TABLE `user`;
+INSERT INTO `user` (create_time, update_time, nickname, balance) VALUES
+(NOW(), NOW(), '小明', 10);
 COMMIT;
 
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+/* -------------------------------- 建库 seabass_product -------------------------------- */
+DROP DATABASE IF EXISTS `seabass_product`;
+CREATE DATABASE  `seabass_product` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE `seabass_product`;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+/* 活动表 */
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `update_time` datetime NOT NULL COMMENT '更新时间',
+    `stock` int DEFAULT NULL COMMENT '库存',
+    `price` DECIMAL(10, 0) NOT NULL DEFAULT 0 COMMENT '价格',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT='产品表';
+
+/* 清空表数据并初始化 */
+BEGIN;
+TRUNCATE TABLE `product`;
+INSERT INTO `product` (create_time, update_time, stock, price) VALUES
+(NOW(), NOW(), 10, 5);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+/* -------------------------------- 建库 seabass_order -------------------------------- */
+DROP DATABASE IF EXISTS `seabass_order`;
+CREATE DATABASE  `seabass_order` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE `seabass_order`;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+/* 活动表 */
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `update_time` datetime NOT NULL COMMENT '更新时间',
+    `user_id` bigint NOT NULL COMMENT '用户Id',
+    `product_id` bigint NOT NULL COMMENT '产品Id',
+    `add_time` datetime NOT NULL COMMENT '下单时间',
+    `pay_amount` DECIMAL(10, 0) NOT NULL DEFAULT 0 COMMENT '支付金额',
+    PRIMARY KEY (`id`),
+    KEY `idx_user_id` (`user_id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COMMENT='订单表';
 
 SET FOREIGN_KEY_CHECKS = 1;
